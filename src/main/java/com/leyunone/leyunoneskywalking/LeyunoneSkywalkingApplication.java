007a17de344b457ec71b25f33a4e734ba92ec2c1
leyunone <365627310@qq.com>
2023-05-15 00:35:22 +0800

    Initial commit

---
+++ src/main/java/com/leyunone/leyunoneskywalking/LeyunoneSkywalkingApplication.java
@@ -0,0 +1,13 @@
+package com.leyunone.leyunoneskywalking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeyunoneSkywalkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeyunoneSkywalkingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leyunone/leyunoneskywalking/LeyunoneSkywalkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leyunone.leyunoneskywalking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeyunoneSkywalkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
